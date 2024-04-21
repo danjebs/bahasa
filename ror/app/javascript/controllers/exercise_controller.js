00d9a8259082a071520cfa35557131929aa41d45
simba <dan.jebamony@gmail.com>
2024-04-21 12:13:29 +0800

    Added collapsible function to exercises

---
+++ ror/app/javascript/controllers/exercise_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = ["collapsible"]
+
+  toggleOpen() {
+    const element = this.collapsibleTarget
+    element.hidden = !element.hidden
+  }
+}
