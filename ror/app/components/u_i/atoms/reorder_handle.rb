87d55cc52ba6422abb433c11dda96dfc56445dd1
simba <dan.jebamony@gmail.com>
2024-05-22 10:04:04 +0800

    Move reorder handle into a VC

---
+++ ror/app/components/u_i/atoms/reorder_handle.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UI::Atoms::ReorderHandle < ViewComponent::Base
+
+  erb_template <<-ERB
+    <button class="reorder-handle cursor-grab bg-white-100 hover:bg-gray-100 dark:hover:bg-gray-700 py-1.5 px-2.5 text-sm font-normal rounded-full">
+      <%= render partial: "shared/icon", locals: { name: 'chevron-up-down' } %>
+    </button>
+  ERB
+
+  def initialize()
+  end
+end
