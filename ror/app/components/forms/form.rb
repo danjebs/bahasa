e52bbd1cf15f622e70978fce95b82d327a5d2fd2
simba <dan.jebamony@gmail.com>
2024-07-16 14:20:15 +0800

    Changed lesson page layout

--- ror/app/components/forms/form.rb
+++ ror/app/components/forms/form.rb
@@ -3,10 +3,12 @@
 class Forms::Form < ViewComponent::Base
   delegate :rich_text_area_tag, to: :helpers
 
-  def initialize(model:, fields:, cancel_href:, delete_href: nil)
+  def initialize(model:, fields:, cancel_href:, delete_href: nil, autofocus: true)
     @model = model
     @fields = fields.compact
     @cancel_href = cancel_href
     @delete_href = delete_href
+
+    @autofocus = autofocus
   end
 end
--- ror/app/components/u_i/atoms/icon_button.rb
+++ ror/app/components/u_i/atoms/icon_button.rb
@@ -14,15 +14,15 @@ class UI::Atoms::IconButton < ViewComponent::Base
     @data = data
 
     @css_size_classes = {
-      sm: "p-1.5 text-sm font-normal rounded-md",
+      sm: "text-sm font-normal rounded-md",
       md: "p-2 text-md font-medium rounded-md",
       lg: "p-3 text-lg font-semibold rounded-lg",
     }[size]
 
     @css_color_classes = {
-      red: "bg-white border-none text-primary-500 hover:bg-gray-100 dark:bg-gray-700 dark:text-gray-300 dark:hover:text-primary-500 dark:hover:bg-gray-600",
+      red: "bg-transparent border-none text-primary-500 hover:bg-gray-100 dark:bg-gray-700 dark:text-gray-300 dark:hover:text-primary-500 dark:hover:bg-gray-600",
       white: "border-2 bg-transparent border-solid border-primary-500 text-primary-500 hover:bg-primary-500 hover:text-white font-semibold",
-      white: "bg-white hover:bg-gray-100 text-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-800",
+      gray: "bg-transparent hover:bg-gray-100 text-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-800",
     }[color]
   end
 end
