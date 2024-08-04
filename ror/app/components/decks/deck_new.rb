20f662d16e14fcb7ea0bb940dfe5cdde06cded2f
simba <dan.jebamony@gmail.com>
2024-08-04 22:08:41 +0800

    Order lessons in deck create by position

--- ror/app/components/decks/deck_new.rb
+++ ror/app/components/decks/deck_new.rb
@@ -5,7 +5,7 @@ class Decks::DeckNew < ViewComponent::Base
   def initialize(deck:)
     @deck = deck
 
-    @step_options = deck.journey.steps.joins(:lesson).pluck("lessons.title", :id)
+    @step_options = deck.journey.steps.joins(:lesson).merge(Lesson.order(position: :asc)).pluck("lessons.title", :id)
     @difficulty_options = Deck.difficulties.keys.map { |difficulty| [difficulty.humanize, difficulty] }
   end
 end
