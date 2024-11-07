fa6d472ab33a4b6904a381977212654b0b190c8f
simba <dan.jebamony@gmail.com>
2024-11-07 10:51:51 +0800

    Add solid cable table

--- ror/db/cable_schema.rb
+++ ror/db/cable_schema.rb
@@ -14,4 +14,13 @@ ActiveRecord::Schema[7.2].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.binary "channel", null: false
+    t.binary "payload", null: false
+    t.datetime "created_at", null: false
+    t.bigint "channel_hash", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+  end
 end
