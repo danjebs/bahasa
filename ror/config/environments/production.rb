c67d53138a9e390828dec5d135fad6b408073f2b
simba <dan.jebamony@gmail.com>
2024-11-07 09:47:48 +0800

    Set up SolidCable

--- ror/config/environments/production.rb
+++ ror/config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
+  config.action_cable.url = "wss://bahasa.kokoro.id/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
