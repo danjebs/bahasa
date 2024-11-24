2a466ea3e4c70645096ff143c45f62df07ae17ec
simba <dan.jebamony@gmail.com>
2024-11-24 13:55:47 +0800

    Set up forbidden routing

--- ror/app/controllers/application_controller.rb
+++ ror/app/controllers/application_controller.rb
@@ -34,9 +34,26 @@ class ApplicationController < ActionController::Base
       { lang: @lang }
     end
 
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
+
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || super
+    end
+
     def user_not_authorized
       flash[:alert] = "You are not authorized to perform this action."
-      redirect_to(request.referrer || root_path)
+
+      if current_user.present?
+        respond_to do |format|
+          format.html { render file: Rails.root.join("public", "403.html"), status: :forbidden, layout: false }
+          format.json { render json: { error: "Forbidden" }, status: :forbidden }
+        end
+      else
+        store_user_location!
+        redirect_to new_user_session_path
+      end
     end
 
     def render_404
