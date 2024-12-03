01dfaaa0a4773eaab6efcc852203c6fa9f4d4c15
simba <dan.jebamony@gmail.com>
2024-12-03 18:48:24 +0800

    Fix homepage redirect for K

--- ror/app/controllers/static_pages_controller.rb
+++ ror/app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     authorize :static_pages, :home?
 
-    if current_user&.default_language.present?
+    if current_user&.courses.last.present?
       redirect_to current_user.courses.last
     elsif current_user.present?
       redirect_to courses_path
